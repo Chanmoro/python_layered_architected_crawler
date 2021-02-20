b89e063621a18819e40d2d952b65144083d8abed
kazuki.morozumi <kazuki.m777@gmail.com>
2021-02-20 16:24:57 +0900

    Change logger to local scope variable

--- crawler/usecases/collect_links.py
+++ crawler/usecases/collect_links.py
@@ -5,15 +5,16 @@ from crawler.domain import Url
 from crawler.repositories.json_repository import JsonRepository
 from crawler.requester.web_page_requester import WebPageRequester
 
+logger = get_logger(__name__)
+
 
 @dataclasses.dataclass(frozen=True)
 class CollectLinks:
-    logger = get_logger(__name__)
     requester: WebPageRequester = dataclasses.field(default_factory=lambda: WebPageRequester())
     repository: JsonRepository = dataclasses.field(default_factory=lambda: JsonRepository())
 
     def exec(self, seed_url: Url, depth: int):
-        self.logger.info(f'seed_url: {seed_url}, depth: {depth}')
+        logger.info(f'seed_url: {seed_url}, depth: {depth}')
         current_depth = 0
         current_depth_urls = [seed_url]
 
@@ -21,10 +22,10 @@ class CollectLinks:
             next_depth_urls = []
             for url in current_depth_urls:
                 if seed_url.domain != url.domain:
-                    self.logger.info(f'Ignore domain which is different from the seed url domain. url: {url}')
+                    logger.info(f'Ignore domain which is different from the seed url domain. url: {url}')
                     continue
 
-                self.logger.info(f'Crawl web page. target url: {url}')
+                logger.info(f'Crawl web page. target url: {url}')
                 web_page = self.requester.get_page(url)
                 self.repository.add(web_page)
 
